8575ab161025b77950cbf865fb6613f3ae0f5173
Samual Norman <me@samual.uk>
2025-06-11 16:54:12 +0100

    ultimate edition

--- packages/match-sorter-utils/src/index.ts
+++ packages/match-sorter-utils/src/index.ts
@@ -21,15 +21,13 @@
 // The remove-accents package is still included as a dependency
 // for attribution purposes, but it will not be imported and bundled.
 
-import { removeAccents } from './remove-accents'
-
 export type AccessorAttributes = {
   threshold?: Ranking
   maxRanking: Ranking
   minRanking: Ranking
 }
 
-export interface RankingInfo {
+export type RankingInfo = MatchRanking & {
   rankedValue: any
   rank: Ranking
   accessorIndex: number
@@ -85,31 +83,29 @@ export function rankItem<TItem>(
 
   if (!options.accessors) {
     // if keys is not specified, then we assume the item given is ready to be matched
-    const rank = getMatchRanking(item as unknown as string, value, options)
+    const matchRanking = getMatchRanking(item as unknown as string, value, options)
     return {
       // ends up being duplicate of 'item' in matches but consistent
       rankedValue: item,
-      rank,
+      ...matchRanking,
       accessorIndex: -1,
       accessorThreshold: options.threshold,
-      passed: rank >= options.threshold,
+      passed: matchRanking.rank >= options.threshold,
     }
   }
 
   const valuesToRank = getAllValuesToRank(item, options.accessors)
-
-  const rankingInfo: RankingInfo = {
-    rankedValue: item,
-    rank: rankings.NO_MATCH as Ranking,
-    accessorIndex: -1,
-    accessorThreshold: options.threshold,
-    passed: false,
-  }
+  let bestMatchRanking: MatchRanking = { rank: rankings.NO_MATCH }
+  let passed = false
+  let accessorIndex = -1
+  let accessorThreshold = options.threshold
+  let rankedValue = item
 
   for (let i = 0; i < valuesToRank.length; i++) {
     const rankValue = valuesToRank[i]!
 
-    let newRank = getMatchRanking(rankValue.itemValue, value, options)
+    let matchRanking = getMatchRanking(rankValue.itemValue, value, options)
+    let newRank = matchRanking.rank
 
     const {
       minRanking,
@@ -125,17 +121,112 @@ export function rankItem<TItem>(
 
     newRank = Math.min(newRank, maxRanking) as Ranking
 
-    if (newRank >= threshold && newRank > rankingInfo.rank) {
-      rankingInfo.rank = newRank
-      rankingInfo.passed = true
-      rankingInfo.accessorIndex = i
-      rankingInfo.accessorThreshold = threshold
-      rankingInfo.rankedValue = rankValue.itemValue
+    if (
+      newRank >= threshold && (
+        newRank > bestMatchRanking.rank || (
+          matchRanking.rank == rankings.MATCHES &&
+            bestMatchRanking.rank == rankings.MATCHES &&
+            matchRanking.closeness > bestMatchRanking.closeness
+        )
+      )
+    ) {
+      bestMatchRanking = matchRanking
+      passed = true
+      accessorIndex = i
+      accessorThreshold = threshold
+      rankedValue = rankValue.itemValue as any
     }
   }
 
-  return rankingInfo
+  return {
+    rankedValue,
+    accessorIndex,
+    accessorThreshold,
+    passed,
+    ...bestMatchRanking
+  }
 }
+
+const similarCharacters = `\
+\\s
+(?:AE|Æ|Ǽ)
+(?:ae|æ|ǽ)
+(?:IJ|Ĳ)
+(?:ij|ĳ)
+(?:OE|Œ)
+(?:oe|œ)
+(?:TH|Þ)
+(?:th|þ)
+(?:A|À|Á|Â|Ã|Ä|Å|Ấ|Ắ|Ẳ|Ẵ|Ặ|Ầ|Ằ|Ȃ|Ā|Ă|Ą|Ǎ|Ǻ|A|Ȁ|A)
+(?:C|Ç|Ḉ|Ć|Ĉ|Ċ|Č|C|Č)
+(?:E|È|É|Ê|Ë|Ế|Ḗ|Ề|Ḕ|Ḝ|Ȇ|Ē|Ĕ|Ė|Ę|Ě|E|Ȅ|Ê|Ȩ|Ɛ)
+(?:I|Ì|Í|Î|Ï|Ḯ|Ȋ|Ĩ|Ī|Ĭ|Į|İ|Ǐ|I|Ȉ|I|Ɨ)
+(?:D|Ð|Ď|Đ|Ḑ)
+(?:N|Ñ|Ń|Ņ|Ň|N|Ǹ)
+(?:O|Ò|Ó|Ô|Õ|Ö|Ø|Ố|Ṍ|Ṓ|Ȏ|Ō|Ŏ|Ő|Ơ|Ǒ|Ǿ|Ồ|Ṑ|Ȍ|O)
+(?:U|Ù|Ú|Û|Ü|Ũ|Ū|Ŭ|Ů|Ű|Ų|Ȗ|Ư|Ǔ|Ǖ|Ǘ|Ǚ|Ǜ|Ứ|Ṹ|Ừ|Ȕ|U)
+(?:Y|Ý|Ŷ|Ÿ|Y|Ỳ|Y)
+(?:a|à|á|â|ã|ä|å|ấ|ắ|ẳ|ẵ|ặ|ầ|ằ|ȃ|ā|ă|ą|ǎ|ǻ|a|ȁ|a)
+(?:c|ç|ḉ|ć|ĉ|ċ|č|c|č)
+(?:e|è|é|ê|ë|ế|ḗ|ề|ḕ|ḝ|ȇ|ē|ĕ|ė|ę|ě|e|ȅ|ê|ȩ|ɛ)
+(?:i|ì|í|î|ï|ḯ|ȋ|ĩ|ī|ĭ|į|ı|ǐ|i|ȉ|i|ɨ)
+(?:d|ð|ď|đ|ḑ)
+(?:n|ñ|ń|ņ|ň|ŉ|n|ǹ)
+(?:o|ò|ó|ô|õ|ö|ø|ố|ṍ|ṓ|ȏ|ō|ŏ|ő|ơ|ǒ|ǿ|ồ|ṑ|ȍ|o)
+(?:u|ù|ú|û|ü|ũ|ū|ŭ|ů|ű|ų|ȗ|ư|ǔ|ǖ|ǘ|ǚ|ǜ|ứ|ṹ|ừ|ȕ|u)
+(?:y|ý|ÿ|ŷ|y|ỳ|y)
+(?:G|Ĝ|Ǵ|Ğ|Ġ|Ģ|Ǧ)
+(?:g|ĝ|ǵ|ğ|ġ|ģ|ǧ)
+(?:H|Ĥ|Ħ|Ḫ|Ȟ|Ḩ)
+(?:h|ĥ|ħ|ḫ|ȟ|ḩ)
+(?:J|Ĵ|J)
+(?:j|ĵ|ǰ)
+(?:K|Ķ|Ḱ|K|Ǩ)
+(?:k|ķ|ḱ|k|ǩ)
+(?:L|Ĺ|Ļ|Ľ|Ŀ)
+(?:l|ĺ|ļ|ľ|ŀ|Ł|ł)
+(?:M|Ḿ|M|M|M)
+(?:m|ḿ|m|m|m)
+(?:P|P|Ṕ|P)
+(?:p|p|ṕ|p)
+(?:R|Ŕ|Ŗ|Ř|R|Ȓ|Ȑ|Ř)
+(?:r|ŕ|ŗ|ř|r|ȓ|ȑ|ř)
+(?:S|Ś|Ŝ|Ş|Ș|Š|Ṥ|Ṧ)
+(?:s|ś|ŝ|ș|ş|š|ſ|ṥ|ṧ)
+(?:T|Ţ|Ț|Ť|Ŧ|T)
+(?:t|ţ|ț|ť|ŧ|t)
+(?:V|V|V)
+(?:v|v|v)
+(?:W|Ŵ|Ẃ|Ẁ|W)
+(?:w|ŵ|ẃ|ẁ|w)
+(?:X|X|X|X|X)
+(?:x|x|x|x|x)
+(?:Z|Ź|Ż|Ž|Z)
+(?:z|ź|ż|ž|z)
+(?:f|ƒ|f)
+(?:Г|Ѓ)
+(?:г|ѓ)
+(?:К|Ќ)
+(?:к|ќ)
+(?:B|B|B)
+(?:b|b|b)
+(?:F|F)
+(?:Q|Q|Q)
+(?:q|q|q)`.split(`\n`)
+
+function searchRegex(search: string) {
+  for (const group of similarCharacters)
+    search = search.replace(new RegExp(group, `g`), group)
+
+  return search
+}
+
+type MatchRanking =
+  { rank: 0 | 6 | 7 } |
+  { rank: 1, index: number, length: number, closeness: number } |
+  { rank: 2, indexes: number[] } |
+  { rank: 3 | 4, index: number, length: number } |
+  { rank: 5, length: number }
 
 /**
  * Gives a rankings score based on how well the two strings match.
@@ -148,75 +239,57 @@ function getMatchRanking<TItem>(
   testString: string,
   stringToRank: string,
   options: RankItemOptions<TItem>
-): Ranking {
-  testString = prepareValueForComparison(testString, options)
-  stringToRank = prepareValueForComparison(stringToRank, options)
+): MatchRanking {
+  let searchString = stringToRank
 
-  // too long
-  if (stringToRank.length > testString.length) {
-    return rankings.NO_MATCH
+  if (!options.keepDiacritics) {
+    searchString = searchRegex(searchString)
   }
 
   // case sensitive equals
-  if (testString === stringToRank) {
-    return rankings.CASE_SENSITIVE_EQUAL
+  if (new RegExp(`^${searchString}$`).test(testString)) {
+    return { rank: rankings.CASE_SENSITIVE_EQUAL }
   }
-
-  // Lower casing before further comparison
-  testString = testString.toLowerCase()
-  stringToRank = stringToRank.toLowerCase()
 
   // case insensitive equals
-  if (testString === stringToRank) {
-    return rankings.EQUAL
+  if (new RegExp(`^${searchString}$`, `i`).test(testString)) {
+    return { rank: rankings.EQUAL }
   }
 
+  let match
+
   // starts with
-  if (testString.startsWith(stringToRank)) {
-    return rankings.STARTS_WITH
+  if (match = new RegExp(`^${searchString}`, `i`).exec(testString)) {
+    return { rank: rankings.STARTS_WITH, length: match[0].length }
   }
 
   // word starts with
-  if (testString.includes(` ${stringToRank}`)) {
-    return rankings.WORD_STARTS_WITH
+  if (match = new RegExp(`\\s${searchString}`, `i`).exec(testString)) {
+    return { rank: rankings.WORD_STARTS_WITH, index: match.index + 1, length: match[0].length - 1 }
   }
 
   // contains
-  if (testString.includes(stringToRank)) {
-    return rankings.CONTAINS
-  } else if (stringToRank.length === 1) {
-    // If the only character in the given stringToRank
-    //   isn't even contained in the testString, then
-    //   it's definitely not a match.
-    return rankings.NO_MATCH
+  // if (testString.includes(stringToRank)) {
+  if (match = new RegExp(searchString, `i`).exec(testString)) {
+    return { rank: rankings.CONTAINS, index: match.index, length: match[0].length }
   }
 
   // acronym
-  if (getAcronym(testString).includes(stringToRank)) {
-    return rankings.ACRONYM
+  if (match = new RegExp(stringToRank.split(``).map(character => `(^|\\s|-)(${searchRegex(character)})(.*)`).join(``)).exec(testString)) {
+    const indexes: number[] = []
+    let index = match.index
+
+    for (const [ arrayIndex, value ] of match.slice(1).entries()) {
+      if (!((arrayIndex + 2) % 3))
+        indexes.push(index)
+
+      index += value.length
+    }
+
+    return { rank: rankings.ACRONYM, indexes }
   }
 
-  // will return a number between rankings.MATCHES and
-  // rankings.MATCHES + 1 depending  on how close of a match it is.
   return getClosenessRanking(testString, stringToRank)
-}
-
-/**
- * Generates an acronym for a string.
- *
- * @param {String} string the string for which to produce the acronym
- * @returns {String} the acronym
- */
-function getAcronym(string: string): string {
-  let acronym = ''
-  const wordsInString = string.split(' ')
-  wordsInString.forEach(wordInString => {
-    const splitByHyphenWords = wordInString.split('-')
-    splitByHyphenWords.forEach(splitByHyphenWord => {
-      acronym += splitByHyphenWord.substr(0, 1)
-    })
-  })
-  return acronym
 }
 
 /**
@@ -232,7 +305,7 @@ function getAcronym(string: string): string {
 function getClosenessRanking(
   testString: string,
   stringToRank: string
-): Ranking {
+): MatchRanking {
   let matchingInOrderCharCount = 0
   let charNumber = 0
   function findMatchingCharacter(
@@ -249,15 +322,9 @@ function getClosenessRanking(
     }
     return -1
   }
-  function getRanking(spread: number) {
-    const spreadPercentage = 1 / spread
-    const inOrderPercentage = matchingInOrderCharCount / stringToRank.length
-    const ranking = rankings.MATCHES + inOrderPercentage * spreadPercentage
-    return ranking as Ranking
-  }
   const firstIndex = findMatchingCharacter(stringToRank[0], testString, 0)
   if (firstIndex < 0) {
-    return rankings.NO_MATCH
+    return { rank: rankings.NO_MATCH }
   }
   charNumber = firstIndex
   for (let i = 1, I = stringToRank.length; i < I; i++) {
@@ -265,12 +332,18 @@ function getClosenessRanking(
     charNumber = findMatchingCharacter(matchChar, testString, charNumber)
     const found = charNumber > -1
     if (!found) {
-      return rankings.NO_MATCH
+      return { rank: rankings.NO_MATCH }
     }
   }
 
-  const spread = charNumber - firstIndex
-  return getRanking(spread)
+  const length = charNumber - firstIndex
+
+  return {
+    rank: rankings.MATCHES,
+    index: length,
+    length: matchingInOrderCharCount,
+    closeness: (matchingInOrderCharCount / stringToRank.length) * (1 / length)
+  }
 }
 
 /**
@@ -281,25 +354,6 @@ function getClosenessRanking(
  */
 export function compareItems<TItem>(a: RankingInfo, b: RankingInfo): number {
   return a.rank === b.rank ? 0 : a.rank > b.rank ? -1 : 1
-}
-
-/**
- * Prepares value for comparison by stringifying it, removing diacritics (if specified)
- * @param {String} value - the value to clean
- * @param {Object} options - {keepDiacritics: whether to remove diacritics}
- * @return {String} the prepared value
- */
-function prepareValueForComparison<TItem>(
-  value: string,
-  { keepDiacritics }: RankItemOptions<TItem>
-): string {
-  // value might not actually be a string at this point (we don't get to choose)
-  // so part of preparing the value for comparison is ensure that it is a string
-  value = `${value}` // toString
-  if (!keepDiacritics) {
-    value = removeAccents(value)
-  }
-  return value
 }
 
 /**

21f97d92338e54c09d650730b05980d756c84141
Samual Norman <me@samual.uk>
2025-06-18 16:28:07 +0100

    fix `exports` field in `jsr.json`

--- scripts/emit-jsr-json.js
+++ scripts/emit-jsr-json.js
@@ -2,13 +2,13 @@
 import { mkdirSync as makeDirectorySync, writeFileSync } from "fs"
 import packageJson from "../package.json" with { type: "json" }
 
-const { name, version, license, exports } = packageJson
+const { name, version, license } = packageJson
 
 makeDirectorySync("dist", { recursive: true })
 
 writeFileSync(
 	"dist/jsr.json",
-	JSON.stringify({ name, version, license, exports }, undefined, "\t")
+	JSON.stringify({ name, version, license, exports: { ".": `./default.ts` } }, undefined, "\t")
 )
 
 process.exit()
